a5d54cbf33316beca133021625e62d44ea1b2c7e
Fatemeh Sabourinia <297432427+fatemehsabourinia@users.noreply.github.com>
2026-07-19 01:23:52 +0100

    fix: align instrumentation package assertion

--- app/src/androidTest/java/com/zaeri/sabourinia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zaeri/sabourinia/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.sabourinia", appContext.packageName)
+        assertEquals("com.zaeri.sabourinia.dogbreedidentifier", appContext.packageName)
     }
 }
